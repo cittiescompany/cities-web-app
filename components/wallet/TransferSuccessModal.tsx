c47e5cdea08161c17b468527e30a90106461f13b
Oluwadunsin <oluwadunsin2016@gmail.com>
2026-02-28 22:20:34 +0100

    add the transaction success modal

---
+++ components/wallet/TransferSuccessModal.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
+// import { useLocation } from "wouter";
+import { CheckCircle, Download, Share2, Home } from "lucide-react";
+import vector from "@/assets/images/Vector.png";
+import { formatCurrency } from "@/lib/utils";
+
+interface TransferSuccessModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  data?: {
+    recipient: string;
+    amount: number;
+    type?: string;
+  }
+}
+
+export default function TransferSuccessModal({ isOpen, onClose, data={recipient:'John Doe', amount:200000, type:'transfer'} }: TransferSuccessModalProps) {
+
+  if (!isOpen) return null;
+
+  return(
+     <div className="fixed inset-0 z-[999] flex items-center justify-center">
+      <div className="absolute inset-0 bg-black opacity-50" onClick={() => onClose()} />
+      <div className="relative w-full max-w-lg mx-4">
+        <Card className="bg-white">
+        <div className="p-8 text-center">
+          {/* Success Icon */}
+          <div className="flex justify-center mb-10">
+            <img src={vector.src} alt="Success Icon" className="w-18 h-18" />
+          </div>
+
+          {/* Success Message */}
+         { data.type === 'transfer' ? (
+           <div>
+          <h1 className="text-2xl font-bold text-gray-900 mb-4">
+            Transfer Successful!
+          </h1>
+          <p className="text-gray-600 mb-8">
+            You have successfully transferred <span className="font-bold">{formatCurrency(data.amount)}</span> to <span className="font-semibold">{data.recipient}</span>
+          </p>
+          </div>
+         ):data.type === "airtime" ? (
+           <div>
+          <h1 className="text-2xl font-bold text-gray-900 mb-4">
+            Airtime Purchase Successful!
+          </h1>
+          <p className="text-gray-600 mb-8">
+            You have successfully sent an airtime of <span className="font-bold">{formatCurrency(data.amount)}</span> to <span className="font-semibold">{data.recipient}</span>
+          </p>
+          </div>
+         ):(
+           <div>
+          <h1 className="text-2xl font-bold text-gray-900 mb-4">
+            Data Purchase Successful!
+          </h1>
+          <p className="text-gray-600 mb-8">
+            You have successfully sent an data of <span className="font-bold">{formatCurrency(data.amount)}</span> to <span className="font-semibold">{data.recipient}</span>
+          </p>
+          </div>
+         )}
+
+
+      <Button
+            onClick={() => onClose()}
+            className="w-full h-12 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-full cursor-pointer flex items-center justify-center gap-2 mt-20"
+          >
+            Cancel
+          </Button>
+       
+        </div>
+      </Card>
+      </div>
+    </div>
+  )
+}
+
